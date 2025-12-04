39e9a9f210d43d9f20a8889980c8c35fc5df2c8a
Marko Fuček <markfuce@gmail.com>
2025-12-04 12:10:40 +0100

    Initial Commit

---
+++ SwiftUILearning/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUILearning
+//
+//  Created by Marko on 04.12.2025..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUILearning/SwiftUILearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILearningApp.swift
+//  SwiftUILearning
+//
+//  Created by Marko on 04.12.2025..
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
